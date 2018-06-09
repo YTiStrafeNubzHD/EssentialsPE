b0dc87ae643d10b5c92024ee2d863b490e9f362b
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2018-06-09 14:20:18 +0200

    another bugfix (untested)

--- src/EssentialsPE/BaseFiles/BaseAPI.php
+++ src/EssentialsPE/BaseFiles/BaseAPI.php
@@ -2208,7 +2208,7 @@ class BaseAPI{
             return false;
         }
         $this->getServer()->getLogger()->debug(TextFormat::YELLOW . "Running EssentialsPE's UpdateFetchTask");
-        $this->getScheduler()->scheduleAsyncTask($task = new UpdateFetchTask($this->getUpdateBuild(), $install));
+        $this->getEssentialsPEPlugin()->getScheduler()->scheduleAsyncTask($task = new UpdateFetchTask($this->getUpdateBuild(), $install));
         $this->updaterTask = $task;
         return true;
     }
